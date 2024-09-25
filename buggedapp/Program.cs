57bcc807bd1eb65765dd057d4b4e1d3d26e05c2d
Bryan Valencia <brvlncia@gmail.com>
2024-09-25 16:30:20 -0500

    debug practice

--- buggedapp/Program.cs
+++ buggedapp/Program.cs
@@ -1,2 +1,22 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using static System.Console;
+
+Write("Enter a number: ");
+double num = double.Parse(ReadLine()!);
+
+double a = 4.5;
+double b = 2.5;
+
+double answer = Add(a, b);
+
+WriteLine($"{a} + {b} = {answer}");
+WriteLine("Press enter...");
+ReadLine();
+WriteLine("Done executing");
+
+// deliberate bug for debugging practice
+double Add(double a, double b)
+{
+    return a + b; 
+}
+
+
