74c24904a081b5ac901a4c7f805df8c8add5ca01
Bryan Valencia <brvlncia@gmail.com>
2024-09-28 02:28:12 -0500

    exception handling and call stack practice

--- buggedapp/Program.cs
+++ buggedapp/Program.cs
@@ -1,22 +1,30 @@
-﻿using static System.Console;
+﻿// using static System.Console;
 
-Write("Enter a number: ");
-double num = double.Parse(ReadLine()!);
+// Write("Enter a number: ");
+// double num = double.Parse(ReadLine()!);
 
-double a = 4.5;
-double b = 2.5;
+// double a = 4.5;
+// double b = 2.5;
 
-double answer = Add(a, b);
+// double answer = Add(a, b);
 
-WriteLine($"{a} + {b} = {answer}");
-WriteLine("Press enter...");
-ReadLine();
-WriteLine("Done executing");
+// WriteLine($"{a} + {b} = {answer}");
+// WriteLine("Press enter...");
+// ReadLine();
+// WriteLine("Done executing");
 
-// deliberate bug for debugging practice
-double Add(double a, double b)
+// // deliberate bug for debugging practice
+// double Add(double a, double b)
+// {
+//     return a + b; 
+// }
+
+static void Withdraw(string accountName, decimal amount)
 {
-    return a + b; 
+    ArgumentException.ThrowIfNullOrWhiteSpace(accountName);
+    ArgumentOutOfRangeException.ThrowIfNegativeOrZero(amount);
+
+    Console.WriteLine($"Withdrew {amount} from {accountName}");
 }
 
-
+Withdraw(accountName: "BTUS", amount: -1500);
---
+++ callstackhandlinglib/Processor.cs
@@ -0,0 +1,18 @@
+﻿using static System.Console;
+
+namespace callstackhandlinglib;
+
+public class Processor
+{
+    public static void Gamma()
+    {
+        WriteLine("In Gamma");
+        Delta();
+    }
+
+    private static void Delta()
+    {
+        WriteLine("In Delta");
+        File.OpenText("uasndaskbdkjlasnd");
+    }
+}
