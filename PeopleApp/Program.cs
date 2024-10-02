475eb371d9fb5dee8586255ca7b8c2df2525cb7c
Bryan Valencia <brvlncia@gmail.com>
2024-10-01 22:33:08 -0500

    type practice

--- PeopleApp/Program.cs
+++ PeopleApp/Program.cs
@@ -2,6 +2,8 @@
 
 ConfigureConsole();
 
+/// Practicing with OOP and generics
+
 Person bob = new();
 
 bob.Name = "Bob Smith";
@@ -11,7 +13,25 @@ bob.Born = new DateTimeOffset(
     hour: 16, minute: 28, second: 39,
     offset: TimeSpan.FromHours(-5));
 
+bob.FavoriteWonder = WondersOfTheAncientWorld.GreatPyramidOfGiza;
+
+bob.BucketList = WondersOfTheAncientWorld.HangingGardensOfBabylon | WondersOfTheAncientWorld.MausoleumAtHalicarnassus;
+
+bob.Children.Add(new() { Name = "Alfred" });
+bob.Children.Add(new() { Name = "Bella" });
+bob.Children.Add(new() { Name = "Zoe" });
+
+Console.WriteLine($"{bob.Name} has {bob.Children.Count} children");
+
+foreach (Person p in bob.Children)
+{
+    Console.WriteLine($"> {p.Name}");
+}
+
+Console.WriteLine($"bobs list: {bob.BucketList}");
+
 Console.WriteLine(bob.Birthday());
+Console.WriteLine(bob.FavWonder());
 
 Person alice = new()
 {
@@ -19,5 +39,26 @@ Person alice = new()
     Born = new(2201, 03, 07, 12, 29, 0, TimeSpan.Zero)
 };
 
-Console.WriteLine(alice.Birthday());
+// Console.WriteLine(alice.Birthday());
+
+Console.WriteLine(Person.Species);
+
+/// Static members practice
+
+BankAccount.InterestRate = 0.012M;
+
+BankAccount jonesAccount = new()
+{
+    AccountName = "Mrs. Jones",
+    Balance = 2400
+};
+
+BankAccount gerrierAccount = new()
+{
+    AccountName = "Ms. Gerrier",
+    Balance = 98
+};
+
+Console.WriteLine($"{jonesAccount.AccountName} earned {(jonesAccount.Balance * BankAccount.InterestRate):C} interest.");
+Console.WriteLine($"{gerrierAccount.AccountName} earned {(gerrierAccount.Balance * BankAccount.InterestRate):C} interest.");
 
--- packtlibrarynetstandard2/Person.cs
+++ packtlibrarynetstandard2/Person.cs
@@ -6,12 +6,21 @@ public class Person : object
 
     public string? Name;
     public DateTimeOffset Born;
+    public WondersOfTheAncientWorld FavoriteWonder;
+    public WondersOfTheAncientWorld BucketList;
+    public List<Person> Children = [];
+    public const string Species = "Homo Sapien";
+    public readonly string HomePlanet = "Earth";
 
     #endregion
 
     public string Birthday()
     {
         return $"{this.Name} was born on {this.Born}";
+    }
+    public string FavWonder()
+    {
+        return $"{this.Name}'s favorite wonder is {this.FavoriteWonder}. Its integer is {(int)this.FavoriteWonder}";
     }
 }
 
